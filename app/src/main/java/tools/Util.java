7645229b0720480b00c3c972f8ae8a350cdc4e80
YOUR FULL NAMEOmid <omidzakeri22@gmail.com>
2020-05-09 17:49:05 +0430

    commited by omid

---
+++ app/src/main/java/tools/Util.java
@@ -0,0 +1,15 @@
+package tools;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Util {
+
+    Map<String, String> params = new HashMap<String, String>();
+
+}
